154268c8147c1d8b00cb17dc5671eb46351d7668
Cyrielle Albert <Cyriellealbert@yahoo.fr>
2022-04-29 10:30:29 +0200

    Matomo npm package place

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
 import { Home, Page2, Page3, Flow } from "./Pages/index";
-import { MatomoProvider, createInstance } from "@datapunt/matomo-tracker-react";
 
-const instance = createInstance({
-  urlBase: "http://localhost:8080/",
-  siteId: 1,
-  userId: "Cyri-test", // optional, default value: `undefined`.
-  disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
-  heartBeat: {
-    // optional, enabled by default
-    active: true, // optional, default value: true
-    seconds: 10, // optional, default value: `15
-  },
-  linkTracking: true, // optional, default value: true
-  configurations: {
-    // optional, default value: {}
-    // any valid matomo configuration, all below are optional
-    disableCookies: true,
-    setSecureCookie: true,
-    setRequestMethod: "POST",
-  },
-});
 function App() {
   return (
-    <MatomoProvider value={instance}>
       <BrowserRouter>
         <div>
           <Switch>
@@ -35,7 +14,6 @@ function App() {
           </Switch>
         </div>
       </BrowserRouter>
-    </MatomoProvider>
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MatomoProvider, createInstance } from "@datapunt/matomo-tracker-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const instance = createInstance({
+  urlBase: "http://localhost:8080/",
+  siteId: 1,
+  userId: "Cyri-test", // optional, default value: `undefined`.
+  disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
+  heartBeat: {
+    // optional, enabled by default
+    active: true, // optional, default value: true
+    seconds: 10, // optional, default value: `15
+  },
+  linkTracking: true, // optional, default value: true
+  configurations: {
+    // optional, default value: {}
+    // any valid matomo configuration, all below are optional
+    disableCookies: true,
+    setSecureCookie: true,
+    setRequestMethod: "POST",
+  },
+});
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MatomoProvider value={instance}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </MatomoProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
